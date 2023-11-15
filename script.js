062ffe6dd200448444a720379697b16eb0456fa2
terga17 <128163440+terga17@users.noreply.github.com>
2023-11-15 18:02:19 +0100

    Sidebar responosive, navbar fixed, parallax removed bc of lag

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menuIcon = document.querySelector(".menu-icon");
+const sidebar = document.querySelector(".sidebar");
 
 menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("change");
+    sidebar.style.width = sidebar.style.width === "400px" ? "0" : "400px";
 });
+
