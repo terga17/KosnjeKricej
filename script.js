884b4f22732cfeac94c87ed8086e7c86eff0034a
terga17 <128163440+terga17@users.noreply.github.com>
2023-11-15 22:45:33 +0100

    Fixed anchor scroll to top bug

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const menuIcon = document.querySelector(".menu-icon");
 const sidebar = document.querySelector(".sidebar");
 
-menuIcon.addEventListener("click", () => {
+menuIcon.addEventListener("click", (e) => {
+    e.preventDefault();
     menuIcon.classList.toggle("change");
     sidebar.style.width = sidebar.style.width === "400px" ? "0" : "400px";
 });
+
 
