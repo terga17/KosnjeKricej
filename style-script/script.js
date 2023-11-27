7643f34e8861689dcf64a439bace2105c885353e
terga17 <128163440+terga17@users.noreply.github.com>
2023-11-27 16:26:17 +0100

    Fixed sidebar text break and added text to Storitve section

--- style-script/script.js
+++ style-script/script.js
@@ -4,7 +4,7 @@ const sidebar = document.querySelector(".sidebar");
 menuIcon.addEventListener("click", (e) => {
     e.preventDefault();
     menuIcon.classList.toggle("change");
-    sidebar.style.width = sidebar.style.width === "400px" ? "0" : "400px";
+    sidebar.style.left = sidebar.style.left === "0px" ? "-400px" : "0px";
 });
 
 const anchors = document.querySelectorAll(".side-anchor");
