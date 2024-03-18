29bddcb396530f89657e54371b47080d982386b9
terga17 <terglav10@gmail.com>
2024-03-18 21:18:51 +0100

    popravki
    phone ref, meta description, call icon start

--- style-script/script.js
+++ style-script/script.js
@@ -8,6 +8,31 @@ document.addEventListener("DOMContentLoaded", function() {
     const faqs = document.querySelectorAll(".vprasanja-list");
     const faqsArr = Array.from(faqs);
 
+    var kontaktDiv = document.querySelector(".contact");
+    var homeNumberDiv = document.querySelector(".home-number");
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function handleVisibility() {
+        if (window.innerWidth <= 760 && isInViewport(kontaktDiv)) {
+            homeNumberDiv.style.display = "none";
+            window.removeEventListener("scroll", handleVisibility);
+        }
+    }
+
+    handleVisibility();
+
+    window.addEventListener("scroll", handleVisibility);
+
+
     if (menuIcon) {
         menuIcon.addEventListener("click", (e) => {
             e.preventDefault();
